e4dc91726bddb5a49aacedba7fd6c2d8d15b5618
4OH4Loop <semperchuck@gmail.com>
2025-02-16 12:21:58 -0500

    Initial Commit

---
+++ DungeonDice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DungeonDice
+//
+//  Created by Carolyn Ballinger on 2/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Carolyn Ballinger on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
